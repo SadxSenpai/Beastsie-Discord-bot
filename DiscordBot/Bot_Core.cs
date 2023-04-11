74830973c86379b0d2c7da46445a88fd7d1e6aa2
SadxSenpai <Marvinhauser14@gmail.com>
2023-04-11 12:25:33 +0200

    Add project files.

---
+++ DiscordBot/Bot_Core.cs
@@ -0,0 +1,52 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    public class Bot_Core
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+
+            using (var fs = File.OpenRead("config.json"))
+                using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+
+            var config = new DiscordConfiguration
+            {
+
+            };
+
+            Client = new DiscordClient(config);
+
+            Client.Ready += OnClientReady;
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+
+            };
+            
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            await Client.ConnectAsync();
+
+            await Task.Delay(1);
+        }
+
+        private Task OnClientReady(object sender, ReadyEventArgs e)
+        {
+            return null;
+        }
+    }
+}
---
+++ DiscordBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot_core = new Bot_Core();
+        }
+    }
+}
